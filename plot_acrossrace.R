3b0e82d8a7164a26b1563ff10943726402d14161
Will Foran <wforan1@andrew.cmu.edu>
2017-06-10 23:29:50 -0400

    nick gets more percission with regex, mk plot funcs

--- plot_acrossrace.R
+++ plot_acrossrace.R
@@ -11,12 +11,14 @@ nickname <- function(n) {
  nicks<-list(
    c('^ \\+',''),
    c(' \\+$',''),
-   c('patrick ','pat ')  ,
-   c('bradley ','brad '),
-   c('rosemary ','rose '),
-   c('jonathan ','jon '),
-   c('rose iacovino','rose foran'),
-   c('kathrine ','katherine '),
+   c('^patrick ','pat ')  ,
+   c('^bradley ','brad '),
+   c('^rosemary ','rose '),
+   c('^jonathan ','jon '),
+   c('^joseph ','joe '),
+   c('^rose iacovino','rose foran'),
+   c('^kathrine ','katherine '),
+   c('^nathan ','nate '),
    c('william ','will ') )
 
  # interively apply each replacemnt
@@ -27,31 +29,48 @@ nickname <- function(n) {
 # count number of years name is seen
 d <-
  read.table('txt/year_person.txt',sep="\t",header=F) %>%
- `names<-`(c('year','name','time')) %>%
- mutate(name=nickname(name),
+ `names<-`(c('year','fullname','time')) %>%
+ mutate(name=nickname(fullname),
         time_s=period_to_seconds(hm(time)) ) %>%
  group_by(name) %>% 
  mutate(nraces=n()) 
 
-
+# by years run
 #d %>% filter(nraces>13) %>% nrow()
-peps <- c('foran','emily mente')
-#          ,'argentine','rachel elder'
-#          ,'steve fie','jon williams'
-#          )
-pepregex <- paste(peps,sep='|',collapse='|')
 
-d.foran <- d %>% filter( grepl(pepregex,name,perl=T ))
-p.foran <-
- ggplot(d.foran) + 
- aes(x=as.factor(year),y=time_s/(60^2),color=name,group=name) +
+
+filterpeps <- function(d,peps) {
+ pepregex <- paste(peps,sep='|',collapse='|')
+ d %>% filter( grepl(pepregex,name,perl=T ))
+}
+
+plot_race <- function(d) {
+ ggplot(d) + 
+ aes(x=as.factor(year),
+     y=time_s/(60^2),
+     color=name,
+     group=name) +
  geom_point() +
  theme_bw()
  #scale_y_continuous(limits=c(18,35))+
+}
+save_img <-function(f,p) ggsave(path.expand('imgs',f),p,width=11,height=5)
 
+
+## 
+p.foran <- d %>% filterpeps(c('foran','emily mente')) %>% plot_race
 pf.line <- p.foran + geom_line()
 pf.lm <- p.foran + geom_smooth(method='lm',se=F) 
 
-print(pf.line)
-ggsave('imgs/foran_family_year.png',pf.line,width=11,height=5)
-ggsave('imgs/foran_family_year_lm.png',pf.lm,width=11,height=5)
+save_img('foran_family_year.png',pf.line)
+save_img('foran_family_year_lm.png',pf.lm)
+
+## 
+
+willcohort <- c('will foran','brad argentine','jon williams','brad frink','joe kopnitsky', 'kayla curtis','aaron walker','rob argentine')
+p.willcohort <- d %>% filterpeps(willcohort) %>% plot_race
+print( p.willcohort + geom_line()  )
+print( p.willcohort + geom_smooth(method='lm',se=F)  )
+
+
+
